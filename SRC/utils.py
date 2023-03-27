37d8421074556ae438570e27949951b3c9378b15
Sushil Deore <sushildeore99@gmail.com>
2023-03-27 07:50:00 -0600

    Hyperparameter tuning

--- SRC/utils.py
+++ SRC/utils.py
@@ -8,6 +8,7 @@ from SRC.exception import CustomException
 from SRC.logger import logging
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     logging.info("Entering Save_object")
@@ -26,7 +27,7 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
 
         report = {}
@@ -34,8 +35,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         for i in range(len(list(models))):
 
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
-            model.fit(X_train, y_train) #Train Model
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train) #Train Model
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
